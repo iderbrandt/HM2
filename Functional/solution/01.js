3e2d310d89ac3d5209f7ccf352087bb33b66234d
Oscar Olsson <happybits@gmail.com>
2021-04-20 15:59:59 +0200

    -

--- Functional/solution/01.js
+++
@@ -1,6 +0,0 @@
-function sum() {
-    let num1 = parseFloat(document.getElementById("num1").value)
-    let num2 = parseFloat(document.getElementById("num2").value)
-    let result = num1 + num2
-    document.getElementById("result").innerText = result
-}
